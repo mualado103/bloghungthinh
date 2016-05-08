30b6437b663baebb2d7fb2ec657811d7741dd51a
mualado103 <mualado84@gmail.com>
2016-05-08 15:22:45 +0700

    Create 6.js

---
+++ 6.js
@@ -0,0 +1,10 @@
+function passCustom(){ 
+	simpleCart.checkoutTo = Custom;
+	simpleCart.email = &quot;manhongit.dhp@gmail.com&quot;
+	simpleCart.currency = VND;
+	simpleCart.successUrl = &quot;http://lakashop123.blogspot.com/&quot;
+} 
+function passGoogle(){ 
+	simpleCart.checkoutTo = Email; 		
+	simpleCart.currency = VND;
+}
